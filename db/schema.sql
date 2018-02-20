46488774e671a6f52d10495316da92bb9e02e0ed
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-02-19 16:17:30 -0800

    Update schema.sql
    TABLE

--- db/schema.sql
+++ db/schema.sql
@@ -5,10 +5,11 @@ CREATE DATABASE burgers_db;
 -- Makes it so all of the following code will affect burgers_db --
 USE burgers_db;
 -- Creates the table "burger_name" within burgers_db --
-CREATE TABLE burger_name (
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+CREATE TABLE burgers (
+  ItemID INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  --add burger 
+  burger_name VARCHAR(20),
   -- Makes a boolean column called "devoured" which cannot contain null --
-  devoured BOOLEAN NOT NULL,
+  devoured BOOLEAN NOT NULL;
 
-  PRIMARY KEY (id)
 );
