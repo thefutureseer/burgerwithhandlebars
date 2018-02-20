2215f6de63de00c283e1e393a2c7865a9730d2f7
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2018-02-19 20:00:56 -0800

    Update seeds.sql
    fix bug

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
 -- Creates new rows containing data in column --
 INSERT INTO burger_name (meat, devoured, topping)
+-- value in each row in order
 VALUES ("beef", TRUE, "bacon"),
  ("chicken", TRUE, "bacon"),
- ("beef", TRUE, "extra cheese")
+ ("beef", TRUE, "extra cheese");
